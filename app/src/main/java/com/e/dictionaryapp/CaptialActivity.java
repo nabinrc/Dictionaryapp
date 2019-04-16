87658dec441768189fc040760078f3365152a3d9
Softwarica College Lab <softwarica@softwarica.com>
2019-04-16 12:16:05 +0545

    intent message

---
+++ app/src/main/java/com/e/dictionaryapp/CaptialActivity.java
@@ -0,0 +1,26 @@
+
+package com.e.dictionaryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CaptialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_captial);
+
+        TextView tvText = findViewById(R.id.tvText);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle!= null){
+            String message = bundle.getString("meaning");
+            tvText.setText(message);
+        }
+        else {
+            Toast.makeText(this,"No message",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
